c329d1b3c333597fa2f2bcd92ce92273497fa319
Nick DiQuattro <ndiquattro@gmail.com>
2015-01-15 09:46:03 -0800

    Cleaned up sample experiment

--- eyetest.py
+++ eyetest.py
@@ -9,35 +9,33 @@ experiment.
 
 # Import modules
 from psychopy import visual
-from psychopy import core, event, gui, monitors, sound
+from psychopy import core, event
 import numpy as np
+
 from pylinkwrapper import eyeLinkFuns  # Here's the special one
 
 # Window set-up
-win = visual.Window(monitor = 'nickMon', units = 'deg',
-                    fullscr = True, allowGUI = False,
-                    color = [0, 1, 0])
+win = visual.Window(monitor = 'nickMon', units = 'deg', fullscr = True,
+                    allowGUI = False, color = 0)
 
 # Initiate eye-tracker link and open EDF
 tracker = eyeLinkFuns(win, '1_test') 
 
-# Calibrate eye-tracker with same background color as experiment
+# Calibrate eye-tracker
 tracker.calibrate()
 
 # Stimulus
 fix = visual.Circle(win, radius = 1, pos = (0, 0), fillColor = [1, 0, 0],
-                    lineColor = -1)
+                    lineColor = [1, 0, 0])
                     
 cfix = visual.Circle(win, radius = .15, fillColor = -1, lineColor = -1)
 
 # Display stimulus 5 times
 for t in range(5):
 
-    # Find random coordinats and set them
-    #fx = np.random.randint(-10, 10)
-    #fy = np.random.randint(-10, 10)
-    fx = 5
-    fy = 0
+    # Find random coordinates and set them
+    fx = np.random.randint(-10, 10)
+    fy = np.random.randint(-10, 10)
     fix.setPos((fx, fy))
    
     # Eye tracker trial set-up
@@ -60,16 +58,20 @@ for t in range(5):
    
     # Wait for response 
     keyp = event.waitKeys()
-    tracker.sendVar('response', keyp[0][0])  # key response to EDF file
-
+    
     # Stop Recording
     tracker.recordOFF()
-    tracker.setTrialResult()  # Ends trial for EDF
+    
+    # Send response key to EDF file
+    tracker.sendVar('response', keyp[0][0])
+
+    # End trial for EDF
+    tracker.setTrialResult()
     
     # ISI with fixation check
     cfix.draw()
     win.flip()
-    tracker.fixcheck(2, 3, 'z')
+    tracker.fixCheck(2, 1, 'z')
 
 # Retrieve EDF
 tracker.endExperiment('C:\\edfs\\')  # Closes and retrieves EDF file to
