8587021dee41d477e62bf9121931335ac5a7f51f
Nick DiQuattro <ndiquattro@gmail.com>
2015-10-23 10:39:34 -0700

    adding setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 class Mock(MagicMock):
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['app', 'pylink']
+MOCK_MODULES = ['pylink']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pylinkwrapper',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Nick DiQuattro',
+    author_email='ndiquattro@gmail.com',
+    description=''
+)
